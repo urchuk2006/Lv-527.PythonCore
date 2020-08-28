26f8460d10c6ba121942bc4e62744fb5e555e855
urchuk2006 <69805255+urchuk2006@users.noreply.github.com>
2020-08-28 13:47:19 +0300

    Create plays_banjo.py

---
+++ plays_banjo.py
@@ -0,0 +1,7 @@
+def areYouPlayingBanjo(name):
+    # Implement me!
+    if name[0] in ('R','r'):
+        return (name + " plays banjo")
+    else:
+        return (name + " does not play banjo")
+        
