0d2967857c4ec154645ad23c93d72617393c11de
urchuk2006 <69805255+urchuk2006@users.noreply.github.com>
2020-09-16 21:17:39 +0300

    Create hw2.py

---
+++ hw2.py
@@ -0,0 +1,7 @@
+a=int(input('Enter the value of A: '))
+b=int(input('Enter the value of B: '))
+print('A + B = ',a+b)
+print('A - B = ',a-b)
+print('A * B = ',a*b)
+print('A / B = ',a/b)
+print('A ** B = ',a**b)
